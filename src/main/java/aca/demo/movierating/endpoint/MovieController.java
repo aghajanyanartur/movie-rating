0b96734bbe48bf8fb8cdd06aca38b81e63109499
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-16 13:17:50 +0400

    Added GetMapping to MovieController

--- src/main/java/aca/demo/movierating/endpoint/MovieController.java
+++ src/main/java/aca/demo/movierating/endpoint/MovieController.java
@@ -2,12 +2,14 @@ package aca.demo.movierating.endpoint;
 
 import aca.demo.movierating.movie.CreateMovie;
 import aca.demo.movierating.movie.Genre;
+import aca.demo.movierating.movie.Movie;
 import aca.demo.movierating.movie.MovieService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
@@ -32,5 +34,17 @@ public class MovieController {
         response.getWriter().println("New Movie Created");
     }
 
+    @GetMapping("movies")
+    public void search(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String genre = request.getParameter("genre").toUpperCase();
+        log.info("Genre - {}", genre);
 
+        var searchResults = movieService.search(Genre.valueOf(genre));
+
+        response.setStatus(200);
+        response.setContentType("text/plain");
+        for(Movie movie : searchResults) {
+            response.getWriter().println(movie);
+        }
+    }
 }
