644ff2accf3e6ce4b4d4d5b9fccfe44de2fcaf09
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-22 20:14:48 +0400

    Changed CreateMovie class

--- src/main/java/aca/demo/movierating/movie/CreateMovie.java
+++ src/main/java/aca/demo/movierating/movie/CreateMovie.java
@@ -1,10 +1,20 @@
 package aca.demo.movierating.movie;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.LocalDate;
 
 @Value
+@Builder
+@Jacksonized
 public class CreateMovie {
 
+    Long id;
     String title;
     Genre genre;
+    LocalDate releasedAt;
+    String director;
+    double rating;
 }
