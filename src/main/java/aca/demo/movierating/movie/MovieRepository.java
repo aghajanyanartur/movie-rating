7edd1c966b872ba68bb4e6da92f5e2be49904253
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-22 20:40:23 +0400

    Changed MovieRepository class, added findById(), persist() and delete() methods

--- src/main/java/aca/demo/movierating/movie/MovieRepository.java
+++ src/main/java/aca/demo/movierating/movie/MovieRepository.java
@@ -13,9 +13,9 @@ public class MovieRepository {
 
     private List<Movie> movies = new ArrayList<>();
 
-    public Optional<Movie> findByTitle(String title) {
-        log.debug("Finding movie by title - {}", title);
-        return movies.stream().filter(m -> m.getTitle().equals(title)).findFirst();
+    public Optional<Movie> findById(Long id) {
+        log.debug("Finding movie by id - {}", id);
+        return movies.stream().filter(m -> m.getId().equals(id)).findFirst();
     }
 
     public List<Movie> findByGenre(Genre genre) {
@@ -23,8 +23,13 @@ public class MovieRepository {
         return movies.stream().filter(m -> m.getGenre() == genre).toList();
     }
 
-    public void save(CreateMovie createMovie) {
-        log.debug("Adding new movie to movies list with createMovie - {}", createMovie);
-        movies.add(new Movie(createMovie));
+    public void persist(Movie movie) {
+        log.debug("Adding new movie to movies list with movie - {}", movie);
+        movies.add(movie);
+    }
+
+    public void delete(Movie movie) {
+        log.debug("Deleting movie from list - {}", movie);
+        movies.remove(findById(movie.getId()).get());
     }
 }
