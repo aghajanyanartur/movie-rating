dd6a9a835644fce83434b24411ace17aed01bbef
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-30 09:29:03 +0400

    Added GeneralExceptionHandler class

---
+++ src/main/java/aca/demo/movierating/endpoint/GeneralExceptionHandler.java
@@ -0,0 +1,30 @@
+package aca.demo.movierating.endpoint;
+
+import aca.demo.movierating.movie.MovieNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GeneralExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(MovieNotFoundException movieNotFoundException) {
+        log.error("Exception - {}", movieNotFoundException);
+        return ResponseEntity.status(400).body(new ErrorResponse(1001, movieNotFoundException.getMessage(), null));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(IllegalArgumentException illegalArgumentException) {
+        log.error("Exception - {}", illegalArgumentException);
+        return ResponseEntity.status(400).body(new ErrorResponse(1002, illegalArgumentException.getMessage(), null));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(RuntimeException runtimeException) {
+        log.error("Exception - {}", runtimeException);
+        return ResponseEntity.status(400).body(new ErrorResponse(1100, runtimeException.getMessage(), null));
+    }
+}
