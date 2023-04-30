b9d08225a831570d05a2f35b40f081b138e4d004
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-30 09:44:58 +0400

    Added CreateReview class

---
+++ src/main/java/aca/demo/movierating/review/CreateReview.java
@@ -0,0 +1,20 @@
+package aca.demo.movierating.review;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.Instant;
+
+@Value
+@Builder
+@Jacksonized
+public class CreateReview {
+
+    Long id;
+    Long movieId;
+    Long userId;
+    String description;
+    double rating;
+    Instant createdAt;
+}
