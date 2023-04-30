943e276a482f97db11bb0004bdc8754b8c244d8b
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-30 09:45:17 +0400

    Added UpdateReview class

---
+++ src/main/java/aca/demo/movierating/review/UpdateReview.java
@@ -0,0 +1,20 @@
+package aca.demo.movierating.review;
+
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.time.Instant;
+
+@Value
+@Builder
+@Jacksonized
+public class UpdateReview {
+
+    Long userId;
+    String description;
+    double rating;
+    Instant createdAt;
+    Instant updatedAt;
+}
