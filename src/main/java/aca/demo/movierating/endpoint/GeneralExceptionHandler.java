869796839188e1e5f2c6d71c950b2d3722332977
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-04-30 11:50:48 +0400

    Added an exception handler for ReviewNotFoundException

--- src/main/java/aca/demo/movierating/endpoint/GeneralExceptionHandler.java
+++ src/main/java/aca/demo/movierating/endpoint/GeneralExceptionHandler.java
@@ -1,6 +1,7 @@
 package aca.demo.movierating.endpoint;
 
 import aca.demo.movierating.movie.MovieNotFoundException;
+import aca.demo.movierating.review.ReviewNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,12 @@ public class GeneralExceptionHandler {
     public ResponseEntity<ErrorResponse> handle(MovieNotFoundException movieNotFoundException) {
         log.error("Exception - {}", movieNotFoundException);
         return ResponseEntity.status(400).body(new ErrorResponse(1001, movieNotFoundException.getMessage(), null));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handle(ReviewNotFoundException reviewNotFoundException) {
+        log.error("Exception - {}", reviewNotFoundException);
+        return ResponseEntity.status(400).body(new ErrorResponse(1003, reviewNotFoundException.getMessage(), null));
     }
 
     @ExceptionHandler
