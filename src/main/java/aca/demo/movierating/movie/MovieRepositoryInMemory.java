32584337698c811fa16135188be0c55c07e2376d
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-14 10:31:24 +0400

    MovieRepository class refactored to MovieRepositoryInMemory implementing MovieRepository interface

---
+++ src/main/java/aca/demo/movierating/movie/MovieRepositoryInMemory.java
@@ -0,0 +1,42 @@
+package aca.demo.movierating.movie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class MovieRepositoryInMemory implements MovieRepository {
+
+    private List<Movie> movies = new ArrayList<>();
+
+    public Optional<Movie> findById(Long id) {
+        log.debug("Finding movie by id - {}", id);
+        return movies.stream().filter(m -> m.getId().equals(id)).findFirst();
+    }
+
+    public List<Movie> search(Genre genre, String title, LocalDate releasedBefore, LocalDate releasedAfter) {
+        log.debug("Finding movies by parameters - genre: {}, title: {}, releasedBefore: {}, releasedAfter: {}",
+                genre, title, releasedBefore, releasedAfter);
+        return movies.stream()
+                .filter(m -> genre == null || m.getGenre() == genre)
+                .filter(m -> title == null || m.getTitle().equals(title))
+                .filter(m -> releasedBefore == null || m.getReleasedAt().isBefore(releasedBefore))
+                .filter(m -> releasedAfter == null || m.getReleasedAt().isAfter(releasedAfter))
+                .toList();
+    }
+
+    public void persist(Movie movie) {
+        log.debug("Adding new movie to movies list with movie - {}", movie);
+        movies.add(movie);
+    }
+
+    public void delete(Movie movie) {
+        log.debug("Deleting movie from list - {}", movie);
+        movies.remove(findById(movie.getId()).get()); // Presence of the movie is checked in MovieService
+    }
+}
