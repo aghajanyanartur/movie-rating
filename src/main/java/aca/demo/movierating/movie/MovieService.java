d87ee233ae8726b70c719add5c0cc2af5bc1ab9c
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-19 06:14:17 +0400

    Added update method, added Transactional annotations on the methods

--- src/main/java/aca/demo/movierating/movie/MovieService.java
+++ src/main/java/aca/demo/movierating/movie/MovieService.java
@@ -3,6 +3,7 @@ package aca.demo.movierating.movie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,17 +17,20 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
+    @Transactional(readOnly = true)
     public Movie getById(Long id) {
         log.debug("MovieService getting movie by id - {}", id);
         return movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE));
     }
 
+    @Transactional(readOnly = true)
     public List<Movie> search(Genre genre, String title, LocalDate releasedBefore, LocalDate releasedAfter) {
         log.debug("MovieService searching movies by parameters - genre: {}, title: {}, releasedBefore: {}, releasedAfter: {}",
                 genre, title, releasedBefore, releasedAfter);
         return movieRepository.search(genre, title, releasedBefore, releasedAfter);
     }
 
+    @Transactional
     public void create(CreateMovie createMovie) {
         log.debug("MovieService creating new movie with createMovie - {}", createMovie);
         if(movieRepository.findById(createMovie.getId()).isPresent()){
@@ -35,12 +39,18 @@ public class MovieService {
         movieRepository.persist(new Movie(createMovie));
     }
 
+    @Transactional
     public void update(Long id, UpdateMovie updateMovie) {
         log.debug("MovieService updating movie with updateMovie - {}", updateMovie);
-        movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE)).update(updateMovie);
+        var movie = getById(id);
+        movie.update(updateMovie);
+        movieRepository.persist(movie);
     }
+
+    @Transactional
     public void delete(Long id) {
         log.debug("MovieService deleting movie by id - {}", id);
-        movieRepository.delete(movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE)));
+        var movie = getById(id);
+        movieRepository.delete(movie);
     }
 }
