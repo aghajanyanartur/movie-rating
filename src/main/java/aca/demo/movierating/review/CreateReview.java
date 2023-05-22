45f0b72ef48fdd2e424b7b9f82f1501b90cfbb3b
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-22 22:16:11 +0400

    Refactored

--- src/main/java/aca/demo/movierating/review/CreateReview.java
+++ src/main/java/aca/demo/movierating/review/CreateReview.java
@@ -1,6 +1,5 @@
 package aca.demo.movierating.review;
 
-import aca.demo.movierating.movie.Movie;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
@@ -13,7 +12,7 @@ import java.time.Instant;
 public class CreateReview {
 
     Long id;
-    Movie movie;
+    Long movieId;
     Long userId;
     String description;
     double rating;
