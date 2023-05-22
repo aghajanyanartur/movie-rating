50e660fd7b13cb27ba5db2fd472f383df832c12c
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-22 22:13:13 +0400

    Removed unused imports

--- src/main/java/aca/demo/movierating/MovieRatingApplication.java
+++ src/main/java/aca/demo/movierating/MovieRatingApplication.java
@@ -1,7 +1,5 @@
 package aca.demo.movierating;
 
-import aca.demo.movierating.movie.Genre;
-import aca.demo.movierating.movie.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class MovieRatingApplication {
-
  	public static void main(String[] args) {
 
 		SpringApplication.run(MovieRatingApplication.class, args);
