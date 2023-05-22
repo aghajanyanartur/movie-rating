503cfd2f302f794d761ae82402c3791e172135fa
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-22 22:22:28 +0400

    Refactored MovieService to use MovieRepositorySpringData

--- src/main/java/aca/demo/movierating/movie/MovieService.java
+++ src/main/java/aca/demo/movierating/movie/MovieService.java
@@ -15,28 +15,36 @@ public class MovieService {
 
     private static final String EXCEPTION_MESSAGE = "Movie not found";
 
-    private final MovieRepository movieRepository;
+//    private final MovieRepository movieRepository;
+    private final MovieRepositorySpringData movieRepositorySpringData;
 
     @Transactional(readOnly = true)
     public Movie getById(Long id) {
         log.debug("MovieService getting movie by id - {}", id);
-        return movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE));
+//        return movieRepository.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE));
+        return movieRepositorySpringData.findById(id).orElseThrow(() -> new MovieNotFoundException(EXCEPTION_MESSAGE));
     }
 
     @Transactional(readOnly = true)
     public List<Movie> search(Genre genre, String title, LocalDate releasedBefore, LocalDate releasedAfter) {
         log.debug("MovieService searching movies by parameters - genre: {}, title: {}, releasedBefore: {}, releasedAfter: {}",
                 genre, title, releasedBefore, releasedAfter);
-        return movieRepository.search(genre, title, releasedBefore, releasedAfter);
+//        return movieRepository.search(genre, title, releasedBefore, releasedAfter);
+        return movieRepositorySpringData.findByGenreOrTitleOrReleasedAtBeforeOrReleasedAtAfter(genre, title, releasedBefore, releasedAfter);
     }
 
     @Transactional
     public void create(CreateMovie createMovie) {
         log.debug("MovieService creating new movie with createMovie - {}", createMovie);
-        if(movieRepository.findById(createMovie.getId()).isPresent()){
+//        if(movieRepository.findById(createMovie.getId()).isPresent()){
+//            throw new IllegalArgumentException();
+//        }
+//        movieRepository.persist(new Movie(createMovie));
+
+        if(movieRepositorySpringData.findById(createMovie.getId()).isPresent()) {
             throw new IllegalArgumentException();
         }
-        movieRepository.persist(new Movie(createMovie));
+        movieRepositorySpringData.save(new Movie(createMovie));
     }
 
     @Transactional
@@ -44,13 +52,15 @@ public class MovieService {
         log.debug("MovieService updating movie with updateMovie - {}", updateMovie);
         var movie = getById(id);
         movie.update(updateMovie);
-        movieRepository.persist(movie);
+//        movieRepository.persist(movie);
+        movieRepositorySpringData.save(movie);
     }
 
     @Transactional
     public void delete(Long id) {
         log.debug("MovieService deleting movie by id - {}", id);
         var movie = getById(id);
-        movieRepository.delete(movie);
+//        movieRepository.delete(movie);
+        movieRepositorySpringData.delete(movie);
     }
 }
