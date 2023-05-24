5fa0db728c547eb9d78c3d179bb5f3de08880555
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-24 20:26:57 +0400

    Changed Component annotation to Service

--- src/main/java/aca/demo/movierating/movie/MovieService.java
+++ src/main/java/aca/demo/movierating/movie/MovieService.java
@@ -2,13 +2,13 @@ package aca.demo.movierating.movie;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Component
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class MovieService {
--- src/main/java/aca/demo/movierating/review/ReviewService.java
+++ src/main/java/aca/demo/movierating/review/ReviewService.java
@@ -4,13 +4,13 @@ import aca.demo.movierating.movie.MovieNotFoundException;
 import aca.demo.movierating.movie.MovieRepositoryJpa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.List;
 
-@Component
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class ReviewService {
