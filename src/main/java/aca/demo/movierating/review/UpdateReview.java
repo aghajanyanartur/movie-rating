1eaa14392b9959ac1a3a23dd9751492e9b2d0de1
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-05-28 12:01:37 +0400

    Moved jakarta validation to UpdateReview class

--- src/main/java/aca/demo/movierating/review/UpdateReview.java
+++ src/main/java/aca/demo/movierating/review/UpdateReview.java
@@ -1,6 +1,10 @@
 package aca.demo.movierating.review;
 
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
@@ -12,8 +16,18 @@ import java.time.Instant;
 @Jacksonized
 public class UpdateReview {
 
+    @NotNull
     Long userId;
+
+    @NotNull
+    @Size(max = 1000)
     String description;
+
+    @NotNull
+    @DecimalMin(value = "0.0")
+    @DecimalMax(value = "10.0")
     Double rating;
+
+    @NotNull
     Instant updatedAt;
 }
