d0081ba2799422cc238930e72df8ed79a40d5190
Artur Aghajanyan <aghajanyan.aa1@gmail.com>
2023-06-04 19:21:17 +0400

    User entity class created

---
+++ src/main/java/aca/demo/movierating/user/User.java
@@ -0,0 +1,21 @@
+package aca.demo.movierating.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+}
